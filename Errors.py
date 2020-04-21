f6b3b73ac7ea73522ea3e5a182e8e62f7c6070a5
pedro.pereira <pedro.pereira@bematech.com.br>
2020-04-21 17:43:05 -0300

    Implemented an error module for the application

---
+++ Errors.py
@@ -0,0 +1,36 @@
+import logging
+
+
+class ApplicationError(Exception):
+    """
+    Generic exception class used by every exceptions implemented
+    """
+    def __init__(self, *args):
+        self.logger = logging.getLogger(__name__)
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        error_message = self.__class__.__name__
+        if self.message:
+            error_message += f": {self.message}"
+
+        self.logger.error(error_message)
+        return error_message
+
+
+class InvalidParameter(ApplicationError):
+    """
+    The InvalidParameter exception is raised when a a invalid parameter is passed to the function
+    """
+
+
+class FileNotFounded(ApplicationError):
+    """
+    The InvalidParameter exception is raised when a a invalid parameter is passed to the function
+    """
+
+
+
